b52814d38402ddc1c9127690f24f597148da7343
krptstc <krptstc@seznam.cz>
2021-04-05 20:52:01 +0200

    Smooth player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,10 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 100
+const MAX_SPEED = 80
+const ACCELARATION = 240
+const FRICTION = ACCELARATION
+
+var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	var inputVector = Vector2.ZERO
@@ -8,6 +12,9 @@ func _physics_process(delta):
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 
-	inputVector = inputVector.normalized()
+	if inputVector != Vector2.ZERO:
+		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELARATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	move_and_slide(inputVector * MAX_SPEED)
+	move_and_slide(velocity)
